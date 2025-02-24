c3c6571f1a4f9fc44a7a9e85b3ae3b15727a9242
Jannes Ri <76713867+JannesRi@users.noreply.github.com>
2025-02-24 23:09:48 +0100

    test: remove index.ts from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
@@ -10,6 +10,10 @@ export default defineConfig({
         coverage: {
             enabled: true,
             provider: 'v8',
+            exclude: [
+                ...(configDefaults.coverage.exclude ?? []),
+                'src/index.ts',
+            ],
         },
     },
 })
