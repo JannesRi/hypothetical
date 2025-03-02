4150fc81f3b764ca95d8d4ce2e3ced336b1feb71
Jannes Ri <76713867+JannesRi@users.noreply.github.com>
2025-03-02 22:43:48 +0100

    chore: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
-    clean: true,
-    sourcemap: true,
     dts: true,
-    minify: true,
+    clean: true,
     tsconfig: 'tsconfig.build.json',
 })
